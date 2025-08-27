df5e343af662ad3534cb28de1ea9d5ccc2a52dc0
Night <mateuzinho4866@gmail.com>
2025-08-27 11:06:18 -0300

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,13 +1,14 @@
 -- ui.lua
-local FluentLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixelpv/KINGHUB/main/libs/fluent.lua"))()
-
 local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixelpv/KINGHUB/main/core/loader.lua"))()
+
+-- Carrega Fluent UI
+local FluentLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixelpv/KINGHUB/main/libs/fluent.lua"))()
 
 local UI = {}
 local MainWindow
 
 function UI:Init(PlayerGui)
-    -- cria janela principal Fluent UI
+    -- cria janela principal
     MainWindow = FluentLib:CreateWindow({
         Name = "KINGHUB - 99 Nights",
         Parent = PlayerGui,
