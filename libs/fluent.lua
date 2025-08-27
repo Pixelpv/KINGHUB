07cfd55aff4f98ec8c0f454189615d1512cc369a
Night <mateuzinho4866@gmail.com>
2025-08-27 11:38:45 -0300

    Update fluent.lua

--- libs/fluent.lua
+++ libs/fluent.lua
@@ -1,9 +1,15 @@
--- Implementação local do Fluent UI
+-- Custom Fluent UI modifications if needed
+-- This file can extend or modify the default Fluent library
 
-local Fluent = {}
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-function Fluent.createWindow(title, options)
-    print(string.format("[Fluent] Criando janela '%s' (%dx%d, tema: %s)", title, options.width, options.height, options.theme))
+-- Add mobile compatibility enhancements
+function Fluent:MobileOptimize()
+    if not iswindowactive then
+        -- Adjust UI for touch devices
+        self.Options.TabWidth = 200
+        self.Options.Size = UDim2.fromOffset(700, 500)
+    end
 end
 
 return Fluent
