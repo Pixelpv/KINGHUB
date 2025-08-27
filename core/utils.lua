d4c461ae5cddb8a806b3254735136d10005b5c26
Night <mateuzinho4866@gmail.com>
2025-08-27 08:30:12 -0300

    Update utils.lua

--- core/utils.lua
+++ core/utils.lua
@@ -1,11 +1,29 @@
--- Funções auxiliares
-
+-- core/utils.lua
 local Utils = {}
 
-function Utils.tableLength(tbl)
-    local count = 0
-    for _ in pairs(tbl) do count = count + 1 end
-    return count
+-- Log formatado
+function Utils:Log(msg)
+    print("[KINGHUB][INFO] " .. msg)
+end
+
+-- Aviso formatado
+function Utils:Warn(msg)
+    warn("[KINGHUB][WARN] " .. msg)
+end
+
+-- Erro formatado
+function Utils:Error(msg)
+    error("[KINGHUB][ERROR] " .. msg)
+end
+
+-- Verifica se uma instância existe
+function Utils:SafeWaitFor(obj, child, timeout)
+    local t = 0
+    while not obj:FindFirstChild(child) and t < (timeout or 5) do
+        task.wait(0.1)
+        t += 0.1
+    end
+    return obj:FindFirstChild(child)
 end
 
 return Utils
