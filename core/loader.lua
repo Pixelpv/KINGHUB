a6dddb50d78868ee159271f915791678137adaa4
Night <mateuzinho4866@gmail.com>
2025-08-27 01:08:47 -0300

    add core/loader.lua

---
+++ core/loader.lua
@@ -0,0 +1,10 @@
+-- Carrega o jogo selecionado
+
+local Loader = {}
+
+function Loader.loadSelectedGame()
+    print("[Loader] Nenhum jogo selecionado para carregar.")
+    -- Aqui você poderia adicionar lógica para escolher e carregar um jogo
+end
+
+return Loader
