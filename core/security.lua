d4b975736beb5a2c9df06491264ab19d465e42a4
Night <mateuzinho4866@gmail.com>
2025-08-27 01:09:08 -0300

    add core/security.lua

---
+++ core/security.lua
@@ -0,0 +1,10 @@
+-- Proteções gerais
+
+local Security = {}
+
+function Security.startProtections()
+    print("[Security] Proteções básicas ativadas.")
+    -- Adicione suas rotinas de proteção aqui
+end
+
+return Security
