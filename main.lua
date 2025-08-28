a9bc576c1205ada7fb9071836d93234fde91231d
Night <mateuzinho4866@gmail.com>
2025-08-27 23:41:17 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,34 @@
--- KINGHUB main.lua
-if not game:IsLoaded() then game.Loaded:Wait() end
+-- KINGHUB Main Loader
+-- Detecta o jogo e carrega o GUI correspondente
 
--- Services
-local Players = game:GetService("Players")
-local HttpService = game:GetService("HttpService")
+local PlaceId = game.PlaceId
+local GameName = game.Name
 
--- Player local
-local LocalPlayer = Players.LocalPlayer
+-- Tabela de jogos suportados
+local Games = {
+    [6016359] = "99Nights",   -- Coloque o PlaceId do jogo 99Nights
+    [12345678] = "Outrojogo"  -- Coloque PlaceId do outro jogo
+}
 
--- Importa o módulo do jogo
-local success, gameModule = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixelpv/KINGHUB/main/games/99Nights.lua"))()
-end)
-
-if not success then
-    warn("[KINGHUB] Failed to load game module:", gameModule)
-    return
+-- Função para carregar script do jogo
+local function LoadGameScript(gameFolder)
+    local scriptPath = ("games/%s/%s.lua"):format(gameFolder, gameFolder)
+    
+    -- Tenta carregar
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/SeuUsuario/KINGHUB/main/"..scriptPath))()
+    end)
+    
+    if success then
+        print("[KINGHUB] GUI do jogo "..gameFolder.." carregado com sucesso!")
+    else
+        warn("[KINGHUB] Erro ao carregar GUI de "..gameFolder..": "..tostring(err))
+    end
 end
 
--- Inicializa
-gameModule.Init()
+-- Detecta e carrega
+if Games[PlaceId] then
+    LoadGameScript(Games[PlaceId])
+else
+    warn("[KINGHUB] Jogo não suportado. PlaceId: "..PlaceId.." Nome: "..GameName)
+end
