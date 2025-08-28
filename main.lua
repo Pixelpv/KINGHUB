e470ef9d2c8e1f2a4662c8bf397b624d3355523e
Night <mateuzinho4866@gmail.com>
2025-08-28 12:40:23 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- main.lua
 
 local Games = {
-    [79546208627805] = "99Nights",
+    [79546208627805] = "99Nights", -- 99 Nights in the Forest
     -- Adicione outros jogos aqui
 }
 
@@ -10,7 +10,7 @@ local gameFolder = Games[placeId]
 
 if gameFolder then
     print("[KINGHUB] Carregando GUI para 99 Nights in the Forest")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixelpv/KINGHUB/main/games/" .. gameFolder .. "/" .. gameFolder .. ".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixelpv/KINGHUB/main/games/" .. gameFolder .. "/" .. gameFolder .. ".lua", true))()
 else
     warn("[KINGHUB] Jogo não suportado. PlaceId:", placeId)
 end
